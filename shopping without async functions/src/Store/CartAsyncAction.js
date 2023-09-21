8148964aac2f8a1a135bdb66b7bb59cb6ee1750c
Yashwardhanm Singh <135504708+YYashSingh-9@users.noreply.github.com>
2023-09-21 03:28:44 -0700

    Delete shopping without async functions directory

--- shopping without async functions/src/Store/CartAsyncAction.js
+++
@@ -1,4 +0,0 @@
-//THIS IS ACTION CREATOR THUNK -> used to execute async functionality in redux toolkit.
-export const sendCartItem = () => {
-  return async (dispatch) => {};
-};
--- shopping without async functions/src/Store/MainStore.jsx
+++
@@ -1,5 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import sliceOne from "./CartSlice";
-const StoreCart = configureStore({ reducer: { cart: sliceOne.reducer } });
-
-export default StoreCart;
